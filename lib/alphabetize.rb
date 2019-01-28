1ad7922d9801bc511f1bb5bcf6f0deaec3dfbc14
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-28 05:17:36 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |sentence|
+    sentence.split(" ").map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end
+  end
 end
